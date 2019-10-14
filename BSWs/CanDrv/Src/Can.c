7042030a9f8fb9a2404d5555100da715b5abc20c
Ahmed Gamal <ahmedgamalag2200@gmail.com>
2019-10-14 19:31:21 +0200

    implementing Can_MainFunction_Write

--- BSWs/CanDrv/Src/Can.c
+++ BSWs/CanDrv/Src/Can.c
@@ -1,158 +1,170 @@
-/********************************************************************************************************************************
-**
-
-FileName:                          Can.c                                    
-AUTOSAR Version:            4.2.2
-																																															**
-********************************************************************************************************************************/
-
-/********************************************************************************************************************************
-**                                                                       includes                                                                                         **
-********************************************************************************************************************************/
-#include "Can.h"
-
-
-
-
-
-
-
-
-
-/*********************************************************************************************************************************       
-Service name:                                       Can_SetControllerMode
-Service ID[hex]:                                                   0x03
-Sync/Async:                                                  Asynchronous
-Reentrancy:                                                  Non Reentrant
-Parameters (in):                                    Controller ---> CAN controller for which the status shall be changed
-                                                               Transition ---> Transition value to request new CAN controller state
-Parameters (inout):                                               None
-Parameters (out):                                                  None
-Return value:                                                Can_ReturnType
-Description:   
-                                  This function performs software triggered state transitions of the CAN controller
-                                  State machine.
+/******************************************************************************************************************************
+Service name:                                             Can_MainFunction_Write
+Service ID[hex]:                                                   0x01
+Description:                    This function performs the polling of TX indications when CAN_TX_PROCESSING is set to POLLING.
 *******************************************************************************************************************************/
-Can_ReturnType    Can_SetControllerMode (uint8 Controller,Can_StateTransitionType Transition) {
-	uint32 cancontrollerbaseadress,Itration=0 ;
-	cancontrollerbaseadress=Can.CanConfigSet.CanController[Controller].CanControllerBaseAddress;
-	if(CanDriverState == CAN_UNINIT){
-		CanDevelopmentError=CAN_E_UNINIT;
-		return CAN_NOT_OK;
-		
-	}
-/**[SWS_Can_00261] [ The function Can_SetControllerMode(CAN_T_START) shall
-set the hardware registers in a way that makes the CAN controller participating on
-the network.]**/
-else	if ( Transition ==CAN_T_START)
-	{
-		if(status_Initialization){
-/**[SWS_Can_00262] [ The function Can_SetControllerMode(CAN_T_START) shall
-wait for limited time until the CAN controller is fully operational. Compare to
-SWS_Can_00398.]**/
-		while( CanTimeoutDuration & status_Initialization){
-			CanTimeoutDuration--;
-			Can_EnableControllerInterrupts(Controller);
-		    CANEnable(cancontrollerbaseadress); 
-		}
-		   return CAN_OK;		   
-		}
-/**[SWS_Can_00409]  When the function Can_SetControllerMode(CAN_T_START)
-is entered and the CAN controller is not in state STOPPED it shall detect a invalid
-state transition (Compare to SWS_Can_00200).**/
-		else{
-			CanDevelopmentError=CAN_E_TRANSITION;
-			return CAN_NOT_OK;
-		}
-	}
-	
-/**	[SWS_Can_00263] [ The function Can_SetControllerMode(CAN_T_STOP) shall set
-the bits inside the CAN hardware such that the CAN controller stops participating on
-the network.]**/
-	else if (Transition ==CAN_T_STOP){
-		if(!status_Initialization){
-/**[SWS_Can_00264] [ The function Can_SetControllerMode(CAN_T_STOP) shall
-wait for a limited time until the CAN controller is really switched off. Compare to
-SWS_Can_00398.]**/
-		while( CanTimeoutDuration & (!status_Initialization)){
-			CanTimeoutDuration--;
-/**[SWS_Can_00282] [ The function Can_SetControllerMode(CAN_T_STOP) shall
-cancel pending messages. ]*/			
-			for(Itration=0x01;Itration<=0x20;Itration){
-			CLEAR_PIN(cancontrollerbaseadress,CANIF1CMSK,7); 
-			CLEAR_PIN(cancontrollerbaseadress,CANIF1CMSK,2); 
-			CLEAR_PIN(cancontrollerbaseadress,CANIF1MCTL,8); 
-			GET_ADDRESS_VAL(cancontrollerbaseadress,CANIF1CRQ) = Itration;
-			}
-			Can_DisableControllerInterrupts(Controller);
-		    CANDisable(cancontrollerbaseadress); 
-		}
-		   return CAN_OK;		   
-		}
-/**[SWS_Can_00410] [ When the function Can_SetControllerMode(CAN_T_STOP) is
-entered and the CAN controller is neither in state STARTED nor in state STOPPED,
-it shall detect a invalid state transition (Compare to SWS_Can_00200).] */
-		else{
-			CanDevelopmentError=CAN_E_TRANSITION;
-			return CAN_NOT_OK;
-		}
-	}
+/********************************************************************************************************************************
+                                                          Type definitions
+********************************************************************************************************************************/
+#define CHECK_BIT(REG,POS) ((REG) & (1<<(POS)))
+/*********************************************************************************************************************************
+                                                             includes
+*********************************************************************************************************************************/
+#include "Can_GeneralTypes.h"
+/********************************************************************************************************************************/
+#if(CanTxProcessing0==INTERRUPT && CanTxProcessing1==INTERRUPT)
+               void Can_MainFunction_Write(){
+                    //empty define function in interrupt mode
+                }
+#elif(CanTxProcessing0==POLLING || CanTxProcessing1==POLLING)
 
-/**[SWS_Can_00265] [ The function Can_SetControllerMode(CAN_T_SLEEP) shall
-set the controller into sleep mode.]**/
-	else if (Transition ==CAN_T_SLEEP){
-/**[SWS_Can_00290] [ If the CAN HW does not support a sleep mode, the function
-Can_SetControllerMode(CAN_T_SLEEP) shall set the CAN controller to the logical
-sleep mode.]**/
-		if(!status_Initialization & ! LogicalSleep[Controller]){
-		while( CanTimeoutDuration & (!status_Initialization)  & !(LogicalSleep[Controller])){
-			CanTimeoutDuration--;
-			Can_DisableControllerInterrupts(Controller);
-		    CANDisable(cancontrollerbaseadress); 
-			LogicalSleep[Controller]=1;
-		}
-		   return CAN_OK;		   
-		}
-/**[SWS_Can_00411] [ When the function Can_SetControllerMode(CAN_T_SLEEP)
-is entered and the CAN controller is neither in state STOPPED nor in state SLEEP, it
-shall detect a invalid state transition]**/
-		else{
-			CanDevelopmentError=CAN_E_TRANSITION;
-			return CAN_NOT_OK;
-		}
-	}
-	
-/**[SWS_Can_00267] [If the CAN HW does not support a sleep mode, the function
-Can_SetControllerMode(CAN_T_WAKEUP) shall return from the logical sleep mode,but have no effect to the CAN controller state (as the controller is already in stopped
-state).]**/
-	else if (Transition ==CAN_T_WAKEUP){
-			
-		if(status_Initialization & LogicalSleep[Controller]){
-/**[SWS_Can_00268] [ The function Can_SetControllerMode(CAN_T_WAKEUP) shall
-wait for a limited time until the CAN controller is in STOPPED state. Compare to
-SWS_Can_00398.]**/
-		while( CanTimeoutDuration & status_Initialization & LogicalSleep[Controller]){
-			CanTimeoutDuration--;
-			Can_EnableControllerInterrupts(Controller);
-		    CANEnable(cancontrollerbaseadress); 
-			LogicalSleep[Controller]=0;
-			
-		}
-		   return CAN_OK;		   
-		}
-/**[SWS_Can_00412]
-[When the function Can_SetControllerMode(CAN_T_WAKEUP) is entered and the CAN controller is
-neither in state SLEEP nor in state STOPPED, it shall detect a invalid state transition
-(Compare to SWS_Can_00200).]**/
-		else{
-			CanDevelopmentError=CAN_E_TRANSITION;
-			return CAN_NOT_OK;
-		}
-	
-	}
-	else {
-		CanDevelopmentError=CAN_E_PARAM_CONTROLLER;
-		return CAN_NOT_OK;
-	}
-	
-}
+    void Can_MainFunction_Write(){
+
+
+        uint8 INDEX_HOH;
+        uint8 INDEX_MESGOBJ;
+
+        #if(CanTxProcessing0==POLLING)
+
+           if(Can.CanConfigSet.CanController[0].CanControllerActivation){
+                for(INDEX_HOH=0;INDEX_HOH<NUMBER_OF_HOH;INDEX_HOH++){
+                    if(Can.CanConfigSet.CanHardwareObject[INDEX_HOH].CanControllerRef== &Can.CanConfigSet.CanController[0]){
+                        if(Can.CanConfigSet.CanHardwareObject[INDEX_HOH].CanObjectType==transmit){
+                          for(INDEX_MESGOBJ=0;INDEX_MESGOBJ<=Can.CanConfigSet.CanHardwareObject[INDEX_HOH].CanHwObjectCount;INDEX_MESGOBJ++){
+                             if(MessageObject[0][INDEX_HOH][INDEX_MESGOBJ].MessageObjectNumber <= 16){
+                                if(!(CHECK_BIT(CAN0_BASE+CAN_O_TXRQ1,(MessageObject[0][INDEX_HOH][INDEX_MESGOBJ].MessageObjectNumber-1)))){
+
+                                    /*[SWS_Can_00016] ⌈The Can module shall call CanIf_TxConfirmation to indicate a
+                                   successful transmission.It shall either called by the TX-interrupt service routine
+                                   of the corresponding HW resource or inside the Can_MainFunction_Write in case of
+                                   polling mode.*/
+
+                                    MessageObject[0][INDEX_HOH][INDEX_MESGOBJ].confirmation=true;
+                                    //CanIf_TxConfirmation()
+
+                                }else{
+
+                                    /*7.8 Notification concept*/
+
+                                    /*CanDrv must store the all in HTHs pending L-PDU Ids in an array
+                                    organized perTH to avoid new search of the L-PDU ID for call of
+
+                                     */
+                                    MessageObject[0][INDEX_HOH][INDEX_MESGOBJ].confirmation=false;
+                                    //CanIf_TxConfirmation(MessageObject[0][INDEX_HOH][INDEX_MESGOBJ].PduId)
+
+                                }
+
+
+                            }else{
+
+                                if(!(CHECK_BIT(CAN0_BASE+CAN_O_TXRQ2,(MessageObject[0][INDEX_HOH][INDEX_MESGOBJ].MessageObjectNumber)-1))){
+
+                                    /*[SWS_Can_00016] ⌈The Can module shall call CanIf_TxConfirmation to indicate a
+                                   successful transmission.It shall either called by the TX-interrupt service routine
+                                   of the corresponding HW resource or inside the Can_MainFunction_Write in case of
+                                   polling mode.*/
+
+                                    MessageObject[0][INDEX_HOH][INDEX_MESGOBJ].confirmation=true;
+                                    //CanIf_TxConfirmation().
+
+                                }else{
+
+                                    /*7.8 Notification concept*/
+
+                                    /*CanDrv must store the all in HTHs pending L-PDU Ids in an array
+                                    organized perTH to avoid new search of the L-PDU ID for call of
+
+                                     */
+                                    MessageObject[0][INDEX_HOH][INDEX_MESGOBJ].confirmation=fasle;
+                                    //CanIf_TxConfirmation(MessageObject[0][INDEX_HOH][INDEX_MESGOBJ].PduId)
+
+                                }
+
+                            }
+
+                          }
+
+                        }else{/*misra error*/}
+                    }else{/*misra error*/}
+                  }
+               }else{/*misra error*/}
+
+               #else //misra error
+               #endif
+
+
+        #if(CanTxProcessing1==POLLING)
+
+           if(Can.CanConfigSet.CanController[1].CanControllerActivation){
+
+                for(INDEX_HOH=0;INDEX_HOH<NUMBER_OF_HOH;INDEX_HOH++){
+                    if(Can.CanConfigSet.CanHardwareObject[INDEX_HOH].CanControllerRef== &Can.CanConfigSet.CanController[1]){
+                        if(Can.CanConfigSet.CanHardwareObject[INDEX_HOH].CanObjectType==transmit){
+                          for(INDEX_MESGOBJ=0;INDEX_MESGOBJ<=Can.CanConfigSet.CanHardwareObject[INDEX_HOH].CanHwObjectCount;INDEX_MESGOBJ++){
+                           //if(MessageObject[0][INDEX_HOH][INDEX_MESGOBJ].MessageObjectNumber){}
+                             if(MessageObject[1][INDEX_HOH][INDEX_MESGOBJ].MessageObjectNumber <= 16){
+                                if(!(CHECK_BIT(CAN1_BASE+CAN_O_TXRQ1,(MessageObject[1][INDEX_HOH][INDEX_MESGOBJ].MessageObjectNumber-1)))){
+
+                                    /*[SWS_Can_00016] ⌈The Can module shall call CanIf_TxConfirmation to indicate a
+                                   successful transmission.It shall either called by the TX-interrupt service routine
+                                   of the corresponding HW resource or inside the Can_MainFunction_Write in case of
+                                   polling mode.*/
+
+                                    MessageObject[1][INDEX_HOH][INDEX_MESGOBJ].confirmation=true;
+                                    //CanIf_TxConfirmation().
+
+                                }else{
+
+                                    /*7.8 Notification concept*/
+
+                                    /*CanDrv must store the all in HTHs pending L-PDU Ids in an array
+                                    organized perTH to avoid new search of the L-PDU ID for call of
+
+                                     */
+                                    MessageObject[1][INDEX_HOH][INDEX_MESGOBJ].confirmation=fasle;
+                                    //CanIf_TxConfirmation(MessageObject[1][INDEX_HOH][INDEX_MESGOBJ].PduId)
+
+                                }
+
+
+                            }else{
+
+                                if(!(CHECK_BIT(CAN1_BASE+CAN_O_TXRQ2,(MessageObject[1][INDEX_HOH][INDEX_MESGOBJ].MessageObjectNumber)-1))){
+
+                                    /*[SWS_Can_00016] ⌈The Can module shall call CanIf_TxConfirmation to indicate a
+                                   successful transmission.It shall either called by the TX-interrupt service routine
+                                   of the corresponding HW resource or inside the Can_MainFunction_Write in case of
+                                   polling mode.*/
+
+                                    MessageObject[1][INDEX_HOH][INDEX_MESGOBJ].confirmation=true;
+                                    //CanIf_TxConfirmation().
+
+                                }else{
+
+                                    /*7.8 Notification concept*/
+
+                                    /*CanDrv must store the all in HTHs pending L-PDU Ids in an array
+                                    organized perTH to avoid new search of the L-PDU ID for call of
+
+                                     */
+
+                                    MessageObject[1][INDEX_HOH][INDEX_MESGOBJ].confirmation=fasle;
+                                    //CanIf_TxConfirmation(MessageObject[1][INDEX_HOH][INDEX_MESGOBJ].PduId)
+
+                                }
+                              }
+                          }
+
+                        }else{/*misra error*/}
+                    }else{/*misra error*/}
+                  }
+               }else{/*misra error*/}
+
+        #else //misra error
+        #endif
+
+        }
+
+#else //misra error
+#endif
