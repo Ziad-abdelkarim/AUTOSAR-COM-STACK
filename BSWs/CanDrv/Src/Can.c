e290a903ef8dd407e526f0c9d1978e58f40419b1
MInaNabilWahib <minanabilsalib@gmail0com>
2019-10-15 15:07:52 +0200

    Can_Writev0.1.1

--- BSWs/CanDrv/Src/Can.c
+++ BSWs/CanDrv/Src/Can.c
@@ -92,145 +92,161 @@ Can_ReturnType Can_Write(Can_HwHandleType Hth,const Can_PduType* PduInfo)
       {
         //  CanDevolpmentErrorType=CAN_E_UNINIT;
             return CAN_NOT_OK;
-      }else{
+      }else
+      {
 
         /*
         7-If default error detection for the Can module is enabled: The function Can_Write shall raise the error CAN_E_PARAM_HANDLE and shall
         return CAN_NOT_OK if the parameter Hth is not a configured Hardware Transmit Handle. ⌋ ()[SWS_Can_00217]
         */
-        const CanHardwareObject* CurrentHObj = Can_GetHardwareObjectOfHTH(Hth);
-        if(CurrentHObj->CanObjectType==Recieve)
-        {
-            //  CanDevolpmentErrorType=CAN_E_PARAM_HANDLE;
-                return CAN_NOT_OK;
-        }else{
-          /*
-          1- The function Can_Write first checks if the hardware transmit object that is identified
-           by the HTH is free and if another Can_Write is ongoing for the same HTH. [SWS_Can_00212]
-          */
-            for(uint8 ObjInd = 0 ; ObjInd < CurrentHObj->CanHwObjectCount;ObjInd++){
-                if(MessageObject[CurrentHObj->CanControllerRef->CanControllerId][CurrentHObj->CanObjectId][ObjInd].confirmation==true)
+                const CanHardwareObject* CurrentHObj = Can_GetHardwareObjectOfHTH(Hth);
+                if(CurrentHObj->CanObjectType==RECEIVE)
                 {
-                  id = ObjInd;
-                  break;
-                }else{
-                  //is not free
-                  /*
-                  3- The function Can_Write shall perform no actions if the hardware transmit object is busy with another transmit request for an L-PDU:
-                  [SWS_Can_00213]
-                        a. The transmission of the other L-PDU shall not be cancelled and the function Can_Write is left without any actions.
-                        b. The function Can_Write shall return CAN_BUSY.⌋ (SRS_Can_01049).
-                  */
-                      return CAN_BUSY;
+                    //  CanDevolpmentErrorType=CAN_E_PARAM_HANDLE;
+                        return CAN_NOT_OK;
+                }else
+                {
+                    /*
+                    1- The function Can_Write first checks if the hardware transmit object that is identified
+                     by the HTH is free and if another Can_Write is ongoing for the same HTH. [SWS_Can_00212]
+                    */
+                    for(uint8 ObjInd = 0 ; ObjInd < CurrentHObj->CanHwObjectCount;ObjInd++)
+                    {
+                          if(MessageObject[CurrentHObj->CanControllerRef->CanControllerId][CurrentHObj->CanObjectId][ObjInd].confirmation==true)
+                          {
+                              id = ObjInd;
+                              break;
+                          }else
+                          {
+                              //is not free
+                              /*
+                              3- The function Can_Write shall perform no actions if the hardware transmit object is busy with another transmit request for an L-PDU:
+                              [SWS_Can_00213]
+                                    a. The transmission of the other L-PDU shall not be cancelled and the function Can_Write is left without any actions.
+                                    b. The function Can_Write shall return CAN_BUSY.⌋ (SRS_Can_01049).
+                              */
+                              return CAN_BUSY;
 
 
-                }
+                          }
 
-            }
-                  //is free
-                  /*
-                  2- if HTH is free :
-                        a. The mutex for that HTH is set to ‘signaled’
-                        b.The ID, DLC and SDU are put in a format appropriate for the hardware (if necessary) and copied in the appropriate
-                          hardware registers/buffers.
-                        c.All necessary control operations to initiate the transmit are done
-                        d.The mutex for that HTH is released
-                        e.The function returns with CAN_OK⌋ (SRS_Can_01049)
-                  */
-                      MessageObject[CurrentHObj->CanControllerRef->CanControllerId][CurrentHObj->CanObjectId][Id].confirmation=false;
-                      /*
-                      9. If default error detection for CanDrv is enabled: Can_Write() shall raise CAN_E_PARAM_POINTER and shall return CAN_NOT_OK
-                      if the parameter PduInfo is a null pointer. ⌋ () [SWS_CAN_00219]
-                      */
-                      if(PduInfo==NULL)
-                      {
+                    }
+                    //is free
+                    /*
+                    2- if HTH is free :
+                          a. The mutex for that HTH is set to ‘signaled’
+                          b.The ID, DLC and SDU are put in a format appropriate for the hardware (if necessary) and copied in the appropriate
+                            hardware registers/buffers.
+                          c.All necessary control operations to initiate the transmit are done
+                          d.The mutex for that HTH is released
+                          e.The function returns with CAN_OK⌋ (SRS_Can_01049)
+                    */
+                    MessageObject[CurrentHObj->CanControllerRef->CanControllerId][CurrentHObj->CanObjectId][Id].confirmation=false;
+                    /*
+                    9. If default error detection for CanDrv is enabled: Can_Write() shall raise CAN_E_PARAM_POINTER and shall return CAN_NOT_OK
+                    if the parameter PduInfo is a null pointer. ⌋ () [SWS_CAN_00219]
+                    */
+                    if(PduInfo==NULL)
+                    {
 
                         //  CanDevolpmentErrorType=CAN_E_PARAM_POINTER;
+                        MessageObject[CurrentHObj->CanControllerRef->CanControllerId][CurrentHObj->CanObjectId][Id].confirmation=true;
+
                         return CAN_NOT_OK;
 
-                      }else{
-                        if(PduInfo->sdu==NULL)
-                        {
-                            /*
-                            12- If default error detection for CanDrv is enabled: Can_Write() shall raise CAN_E_PARAM_POINTER and shall return CAN_NOT_OK
-                            if the trigger transmit API is disabled for this hardware object
-                            (CanTriggerTransmitEnable = FALSE) and the SDU pointer inside PduInfo is a null pointer. ⌋ ()[SWS_CAN_00505]
-                            */
-                            if(CurrentHObj->CanTriggerTransmitEnable==false)
+                    }else
+                    {
+                            if(PduInfo->sdu==NULL)
                             {
-                                //CanDevolpmentErrorType=CAN_E_PARAM_POINTER;
-                              return Can_NOT_OK;
-                            }else{
                                 /*
-                                10- Can_Write() shall accept a null pointer as SDU (Can_PduType.Can_SduPtrType = NULL) if the trigger transmit API is enabled
-                                for this hardware object (CanTriggerTransmitEnable = TRUE). ⌋ ()[SWS_CAN_00503]
+                                12- If default error detection for CanDrv is enabled: Can_Write() shall raise CAN_E_PARAM_POINTER and shall return CAN_NOT_OK
+                                if the trigger transmit API is disabled for this hardware object
+                                (CanTriggerTransmitEnable = FALSE) and the SDU pointer inside PduInfo is a null pointer. ⌋ ()[SWS_CAN_00505]
                                 */
-                                /*
-                                11- If the trigger transmit API is enabled for the hardware object, Can_Write() shall interpret a null pointer as SDU
-                                (Can_PduType.Can_SduPtrType = NULL) as request for using the trigger transmit interface.
-                                If so and the hardware object is free, Can_Write() shall call CanIf_TriggerTransmit() to acquire the PDU’s data. ⌋ ()[SWS_CAN_00504]
-                                */
-                                if(MessageObject[CurrentHObj->CanControllerRef->CanControllerId][CurrentHObj->CanObjectId][Id].confirmation==true){
-                                  //nadyi 3ala el api mn canif bta3t el transmitapi
+                                if(CurrentHObj->CanTriggerTransmitEnable==false)
+                                {
+                                    MessageObject[CurrentHObj->CanControllerRef->CanControllerId][CurrentHObj->CanObjectId][Id].confirmation=true;
+                                    //CanDevolpmentErrorType=CAN_E_PARAM_POINTER;
+                                    return Can_NOT_OK;
+                                }else
+                                {
+                                      /*
+                                      10- Can_Write() shall accept a null pointer as SDU (Can_PduType.Can_SduPtrType = NULL) if the trigger transmit API is enabled
+                                      for this hardware object (CanTriggerTransmitEnable = TRUE). ⌋ ()[SWS_CAN_00503]
+                                      */
+                                      /*
+                                      11- If the trigger transmit API is enabled for the hardware object, Can_Write() shall interpret a null pointer as SDU
+                                      (Can_PduType.Can_SduPtrType = NULL) as request for using the trigger transmit interface.
+                                      If so and the hardware object is free, Can_Write() shall call CanIf_TriggerTransmit() to acquire the PDU’s data. ⌋ ()[SWS_CAN_00504]
+                                      */
+                                      if(MessageObject[CurrentHObj->CanControllerRef->CanControllerId][CurrentHObj->CanObjectId][Id].confirmation==true)
+                                      {
+                                        //nadyi 3ala el api mn canif bta3t el transmitapi
 
-                                  /*
-                                  13- If default error detection for CanDrv is enabled:Can_Write() shall raise CAN_E_PARAM_POINTER and shall return CAN_NOT_OK
-                                  if the trigger transmit API (CanIf_TriggerTransmit()) returns E_NOT_OK. ⌋ ()[SWS_CAN_00506]
-                                  */
+                                        /*
+                                        13- If default error detection for CanDrv is enabled:Can_Write() shall raise CAN_E_PARAM_POINTER and shall return CAN_NOT_OK
+                                        if the trigger transmit API (CanIf_TriggerTransmit()) returns E_NOT_OK. ⌋ ()[SWS_CAN_00506]
+                                        */
 
-                                }else{
+                                      }else
+                                      {
+                                        //ab2o shkolfko 7aga wla ayi btangan hna
+
+                                      }
+
 
                                 }
 
+                            }else
+                            {
+
+                                  /*
+                                  8- The function Can_Write shall return CAN_NOT_OK and if default error detection for the CAN module is enabled shall raise the error
+                                  CAN_E_PARAM_DLC: [SWS_Can_00218]
+
+                                        a. If the length is more than 64 byte.
+                                        b. If the length is more than 8 byte and the CAN controller is not in CAN FD mode (no CanControllerFdBaudrateConfig).
+                                        c. If the length is more than 8 byte and the CAN controller is in CAN FD mode (valid CanControllerFdBaudrateConfig),
+                                            but the CAN FD flag in Can_PduType->id is not set (refer Can_IdType).⌋ ( SRS_Can_01005)
+                                  */
+                                  // full
+                                  if(PduInfo->length<=8u)
+                                  {
+                                          // send data
+                                          MessageObject[CurrentHObj->CanControllerRef->CanControllerId][CurrentHObj->CanObjectId][Id].PduId=PduInfo->swPduHandle;
+                                          sCANMessage.ui32MsgID = PduInfo->id;
+                                          sCANMessage.ui32MsgLen = PduInfo->length;
+                                                      /*if(PduInfo->length<1U)
+                                                      {
+                                                            sCANMessage.ui32MsgLen = 1U;
+                                                      }
+
+                                                      else{
+                                                          sCANMessage.ui32MsgLen=PduInfo->length;
+
+                                                        }*/
+                                          sCANMessage.pui8MsgData =PduInfo->sdu;
+                                          sCANMessage.ui32MsgIDMask = 0U;
+                                          if(CurrentHObj->CanIdType==EXTENDED)
+                                          {
+                                              sCANMessage.ui32Flags = MSG_OBJ_TX_INT_ENABLE|MSG_OBJ_EXTENDED_ID;
+                                          }else
+                                          {
+                                                sCANMessage.ui32Flags = MSG_OBJ_TX_INT_ENABLE;
+                                          }
+                                          CANMessageSet(CurrentHObj->CanControllerRef->CanControllerBaseAddress,
+                                          MessageObject[CurrentHObj->CanControllerRef->CanControllerId][CurrentHObj->CanObjectId][Id].MessageObjectNumber, &sCANMessage, MSG_OBJ_TYPE_TX);
+                                          MessageObject[CurrentHObj->CanControllerRef->CanControllerId][CurrentHObj->CanObjectId][Id].confirmation=true;
+                                          return  CAN_OK;
+
+                                  }else
+                                  {
+                                          // CanDevolpmentErrorType=CAN_E_PARAM_DLC
+                                          MessageObject[CurrentHObj->CanControllerRef->CanControllerId][CurrentHObj->CanObjectId][Id].confirmation=true;
+                                          return CAN_NOT_OK
+                                  }
 
                             }
-
-                        }else{
-
-                              /*
-                              8- The function Can_Write shall return CAN_NOT_OK and if default error detection for the CAN module is enabled shall raise the error
-                              CAN_E_PARAM_DLC: [SWS_Can_00218]
-
-                                    a. If the length is more than 64 byte.
-                                    b. If the length is more than 8 byte and the CAN controller is not in CAN FD mode (no CanControllerFdBaudrateConfig).
-                                    c. If the length is more than 8 byte and the CAN controller is in CAN FD mode (valid CanControllerFdBaudrateConfig),
-                                        but the CAN FD flag in Can_PduType->id is not set (refer Can_IdType).⌋ ( SRS_Can_01005)
-                              */
-                              // full
-                              if(PduInfo->length<8u){
-                                // send data
-                                MessageObject[CurrentHObj->CanControllerRef->CanControllerId][CurrentHObj->CanObjectId][Id].PduId=swPduHandle;
-                                sCANMessage.ui32MsgID = PduInfo->id;
-                                sCANMessage.ui32MsgLen = PduInfo->length;
-                                if(PduInfo->length<1U)
-                                            {
-                                                sCANMessage.ui32MsgLen = 1U;
-                                            }
-
-                                            else{
-                                                sCANMessage.ui32MsgLen=PduInfo->length;
-
-                                                }
-                                            sCANMessage.pui8MsgData =PduInfo->sdu;
-                                            sCANMessage.ui32MsgIDMask = 0U;
-                                            if(CurrentHObj->CanIdType==EXTENDED)
-                                            {
-                                                sCANMessage.ui32Flags = MSG_OBJ_TX_INT_ENABLE|MSG_OBJ_EXTENDED_ID;
-                                            }else{
-
-                                                sCANMessage.ui32Flags = MSG_OBJ_TX_INT_ENABLE;
-                                                 }
-                                            CANMessageSet(CurrentHObj->CanControllerRef->CanControllerBaseAddress,
-                                            MessageObject[CurrentHObj->CanControllerRef->CanControllerId][CurrentHObj->CanObjectId][Id].MessageObjectNumber, &sCANMessage, MSG_OBJ_TYPE_TX);
-                                            MessageObject[CurrentHObj->CanControllerRef->CanControllerId][CurrentHObj->CanObjectId][Id].confirmation=true;
-                                              return  CAN_OK;
-
-                              }else{
-                                // CanDevolpmentErrorType=CAN_E_PARAM_DLC
-                                return CAN_NOT_OK
-                              }
-
-                        }
 
                       }
 
@@ -243,7 +259,7 @@ Can_ReturnType Can_Write(Can_HwHandleType Hth,const Can_PduType* PduInfo)
 
 
 
-        }
+              }
 
 
       }
