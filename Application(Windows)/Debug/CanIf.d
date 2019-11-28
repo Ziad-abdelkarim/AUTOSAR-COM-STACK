d207329def542856df580a9ccc1dc4680d26016b
ziad abdelkarim <ziad.abdelkarim1997@gmail.com>
2019-11-28 16:19:43 +0200

    CanIf V0.1.6

---
+++ Application(Windows)/Debug/CanIf.d
@@ -0,0 +1,77 @@
+# FIXED
+
+CanIf.obj: E:/Engineering/Graduation//\ Project/Adaptive-Cruise-Control/BSWs/CanIf/Src/CanIf.c
+CanIf.obj: E:/Engineering/Graduation//\ Project/Adaptive-Cruise-Control/BSWs/CanDrv/Inc/Can.h
+CanIf.obj: E:/Engineering/Graduation//\ Project/Adaptive-Cruise-Control/BSWs/CanDrv/Inc/Can_GeneralTypes.h
+CanIf.obj: E:/Engineering/Graduation//\ Project/Adaptive-Cruise-Control/BSWs/CanDrv/Inc/ComStack_Types.h
+CanIf.obj: E:/Engineering/Graduation//\ Project/Adaptive-Cruise-Control/BSWs/CanDrv/Inc/Std_Types.h
+CanIf.obj: E:/Engineering/Graduation//\ Project/Adaptive-Cruise-Control/BSWs/CanDrv/Inc/Platform_Types.h
+CanIf.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdint.h
+CanIf.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_stdint40.h
+CanIf.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/stdint.h
+CanIf.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/cdefs.h
+CanIf.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_types.h
+CanIf.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_types.h
+CanIf.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_stdint.h
+CanIf.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_stdint.h
+CanIf.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdbool.h
+CanIf.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stddef.h
+CanIf.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_ti_config.h
+CanIf.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/linkage.h
+CanIf.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h
+CanIf.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h
+CanIf.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h
+CanIf.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_can.h
+CanIf.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h
+CanIf.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/can.h
+CanIf.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h
+CanIf.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h
+CanIf.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h
+CanIf.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/debug.h
+CanIf.obj: C:/ti/TivaWare_C_Series-2.1.4.178/utils/uartstdio.h
+CanIf.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdarg.h
+CanIf.obj: E:/Engineering/Graduation//\ Project/Adaptive-Cruise-Control/BSWs/CanIf/Inc/CanIf.h
+CanIf.obj: E:/Engineering/Graduation//\ Project/Adaptive-Cruise-Control/BSWs/CanIf/Gen/CanIf_Cfg.h
+CanIf.obj: E:/Engineering/Graduation//\ Project/Adaptive-Cruise-Control/BSWs/CanIf/Inc/CanIf_Types.h
+CanIf.obj: E:/Engineering/Graduation//\ Project/Adaptive-Cruise-Control/BSWs/CanDrv/Gen/Can_Cfg.h
+CanIf.obj: E:/Engineering/Graduation//\ Project/Adaptive-Cruise-Control/BSWs/PduR/Inc/PduR.h
+CanIf.obj: E:/Engineering/Graduation//\ Project/Adaptive-Cruise-Control/BSWs/CanSM/Inc/CanSM.h
+CanIf.obj: E:/Engineering/Graduation//\ Project/Adaptive-Cruise-Control/BSWs/Det/Inc/Det.h
+
+E:/Engineering/Graduation//\ Project/Adaptive-Cruise-Control/BSWs/CanIf/Src/CanIf.c: 
+E:/Engineering/Graduation//\ Project/Adaptive-Cruise-Control/BSWs/CanDrv/Inc/Can.h: 
+E:/Engineering/Graduation//\ Project/Adaptive-Cruise-Control/BSWs/CanDrv/Inc/Can_GeneralTypes.h: 
+E:/Engineering/Graduation//\ Project/Adaptive-Cruise-Control/BSWs/CanDrv/Inc/ComStack_Types.h: 
+E:/Engineering/Graduation//\ Project/Adaptive-Cruise-Control/BSWs/CanDrv/Inc/Std_Types.h: 
+E:/Engineering/Graduation//\ Project/Adaptive-Cruise-Control/BSWs/CanDrv/Inc/Platform_Types.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdint.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_stdint40.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/stdint.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/cdefs.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_types.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_types.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_stdint.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_stdint.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdbool.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stddef.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_ti_config.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/linkage.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_can.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/can.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/debug.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/utils/uartstdio.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdarg.h: 
+E:/Engineering/Graduation//\ Project/Adaptive-Cruise-Control/BSWs/CanIf/Inc/CanIf.h: 
+E:/Engineering/Graduation//\ Project/Adaptive-Cruise-Control/BSWs/CanIf/Gen/CanIf_Cfg.h: 
+E:/Engineering/Graduation//\ Project/Adaptive-Cruise-Control/BSWs/CanIf/Inc/CanIf_Types.h: 
+E:/Engineering/Graduation//\ Project/Adaptive-Cruise-Control/BSWs/CanDrv/Gen/Can_Cfg.h: 
+E:/Engineering/Graduation//\ Project/Adaptive-Cruise-Control/BSWs/PduR/Inc/PduR.h: 
+E:/Engineering/Graduation//\ Project/Adaptive-Cruise-Control/BSWs/CanSM/Inc/CanSM.h: 
+E:/Engineering/Graduation//\ Project/Adaptive-Cruise-Control/BSWs/Det/Inc/Det.h: 
---
+++ Application(Windows)/Debug/PduR.d
@@ -0,0 +1,61 @@
+# FIXED
+
+PduR.obj: E:/Engineering/Graduation//\ Project/Adaptive-Cruise-Control/BSWs/PduR/Src/PduR.c
+PduR.obj: E:/Engineering/Graduation//\ Project/Adaptive-Cruise-Control/BSWs/PduR/Inc/PduR.h
+PduR.obj: E:/Engineering/Graduation//\ Project/Adaptive-Cruise-Control/BSWs/CanDrv/Inc/Platform_Types.h
+PduR.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdint.h
+PduR.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_stdint40.h
+PduR.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/stdint.h
+PduR.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/cdefs.h
+PduR.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_types.h
+PduR.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_types.h
+PduR.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_stdint.h
+PduR.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_stdint.h
+PduR.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdbool.h
+PduR.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stddef.h
+PduR.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_ti_config.h
+PduR.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/linkage.h
+PduR.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h
+PduR.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h
+PduR.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h
+PduR.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_can.h
+PduR.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h
+PduR.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/can.h
+PduR.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h
+PduR.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h
+PduR.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h
+PduR.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/debug.h
+PduR.obj: C:/ti/TivaWare_C_Series-2.1.4.178/utils/uartstdio.h
+PduR.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdarg.h
+PduR.obj: E:/Engineering/Graduation//\ Project/Adaptive-Cruise-Control/BSWs/CanDrv/Inc/ComStack_Types.h
+PduR.obj: E:/Engineering/Graduation//\ Project/Adaptive-Cruise-Control/BSWs/CanDrv/Inc/Std_Types.h
+
+E:/Engineering/Graduation//\ Project/Adaptive-Cruise-Control/BSWs/PduR/Src/PduR.c: 
+E:/Engineering/Graduation//\ Project/Adaptive-Cruise-Control/BSWs/PduR/Inc/PduR.h: 
+E:/Engineering/Graduation//\ Project/Adaptive-Cruise-Control/BSWs/CanDrv/Inc/Platform_Types.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdint.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_stdint40.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/stdint.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/cdefs.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_types.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_types.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_stdint.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_stdint.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdbool.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stddef.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_ti_config.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/linkage.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_can.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/can.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/debug.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/utils/uartstdio.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdarg.h: 
+E:/Engineering/Graduation//\ Project/Adaptive-Cruise-Control/BSWs/CanDrv/Inc/ComStack_Types.h: 
+E:/Engineering/Graduation//\ Project/Adaptive-Cruise-Control/BSWs/CanDrv/Inc/Std_Types.h: 
